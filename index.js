32bba9588fd399f00ab6ab4047675b8c4766e83d
Anshul Kharbanda <akanshul97@gmail.com>
2026-01-05 04:26:44 +0000

    Convert js code from Copilot to rust code

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-function main(pkg) {
-    const a = BigInt(Math.round(10 * Math.random()))
-    const b = BigInt(Math.round(10 * Math.random()))
-    const c = pkg.add(a, b)
-
-    const root = document.createElement('div');
-    root.style.fontFamily = 'Arial, sans-serif';
-    root.style.padding = '24px';
-    root.innerHTML = `
-    <h1>Rust WASM Double Pendulum</h1>
-    <p>Webpack + HtmlWebpackPlugin demo. Replace this with your app entry.</p>
-    <p>${a} + ${b} = ${c}</p>
-    `;
-
-    document.body.appendChild(root);
-}
-
-require('./pkg').then(pkg => main(pkg))
+import('./pkg').catch(console.error)
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,46 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[allow(unused_macros)]
+macro_rules! console_log {
+    ($($t:tt)*) => {
+        web_sys::console::log_1(&format!($($t)*).into())
+    };
+}
+
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+
+    // Get document
+    let document = web_sys::window()
+        .ok_or(JsValue::from_str("Could not find window object!"))?
+        .document()
+        .ok_or(JsValue::from_str("Could not find webpage document!"))?;
+
+    // Simple arithmetic demo
+    let a = 8;
+    let b = 10;
+    let c = a + b;
+
+    // Create root element
+    let root = document.create_element("div")?;
+    root.set_attribute("style", "font-family:Arial,sans-serif;padding:24px;")?;
+
+    // Create html substring
+    let mut subhtml = String::new();
+    subhtml.push_str("<h1>Rust WASM Double Pendulum</h1>\n");
+    subhtml
+        .push_str("<p>Webpack + HtmlWebpackPlugin demo. Replace this with your app entry.</p>\n");
+    subhtml.push_str(&format!("<p>{} + {} = {}</p>", a, b, c));
+
+    // Set substring in root
+    root.set_inner_html(&subhtml);
+
+    // Add root to document body
+    document
+        .body()
+        .ok_or(JsValue::from("Could not find document body!"))?
+        .append_child(&root)?;
+
+    Ok(())
 }
